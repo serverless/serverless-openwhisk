cf2c01b6bd217c6159efa0d16e950728da0fdad0
ac360 <austen@serverless.com>
2016-11-09 18:01:09 -0800

    add tests

---
+++ tests/all.js
@@ -0,0 +1,9 @@
+// OpenWhisk Plugins Tests
+require('../lib/compile/triggers/tests');
+require('../lib/compile/functions/tests');
+require('../lib/deploy/tests');
+require('../lib/deploy/tests/all');
+require('../lib/invoke/tests');
+require('../lib/remove/tests');
+require('../lib/remove/tests/all');
+require('../lib/util/tests');
