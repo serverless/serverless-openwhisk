868ffc8fb352d84ed23e66b4be45ddf2462a03aa
James Thomas <james.thomas@uk.ibm.com>
2016-11-18 10:52:43 +0000

    Updating openwhisk provider plugin to new format.
    Added module main file, index.js, which loads all
    the plugins.

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/*
+NOTE: this plugin is used to add all the differnet provider related plugins at once.
+This way only one plugin needs to be added to the service in order to get access to the
+whole provider implementation.
+*/
+
+const CompileFunctions = require('./compile/functions/index.js');
+const CompileTriggers = require('./compile/triggers/index.js');
+const Deploy = require('./deploy/index.js');
+const Invoke = require('./invoke/index.js');
+const Remove = require('./remove/index.js');
+
+class Index {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.serverless.pluginManager.addPlugin(CompileFunctions);
+    this.serverless.pluginManager.addPlugin(CompileTriggers);
+    this.serverless.pluginManager.addPlugin(Remove);
+    this.serverless.pluginManager.addPlugin(Invoke);
+    this.serverless.pluginManager.addPlugin(Deploy);
+  }
+}
+
+module.exports = Index;
