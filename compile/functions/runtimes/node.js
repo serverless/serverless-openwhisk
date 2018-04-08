5f624b964ff77fd73ccc666bf1180f255ceef02d
Kevin Mitchell <kevin@infielddesign.com>
2018-04-08 16:46:57 -0500

    Allow TypeScript file extension to pass the file exists nodejs runtime check
    Out of the box the Base runtime class will check to see if a given source handler exists. This uses the "extension" value for the given runtime (e.g. `this.extension = ".js"` in the case of nodejs. Because this value is hardcoded to a single value, if transpiling TypeScript using the `serverless-plugin-typescript` plugin for example the `isValidFile` check will fail because there will be no handler file with the given path because it is just assumed that the file will end with ".js".
    
    This commit adds a check specifically for a `.ts` file if the check for the `.js` file fails.

--- compile/functions/runtimes/node.js
+++ compile/functions/runtimes/node.js
@@ -17,6 +17,25 @@ class Node extends BaseRuntime {
     zip.file("package.json", JSON.stringify({main: handlerFile}))
     return zip
   }
+
+  //We're handling a special case here which is that if TypeScript is being used
+  //we won't actually have a ".js" file (this.extension), instead we'll have a "ts"
+  //which should still be considered safe enough, or at least shouldn't
+  //completely stop the deployment process
+  isValidFile(handlerFile) {
+    if (fs.existsSync(handlerFile) || this.isValidTypeScriptFile(handlerFile)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  //Check for TypeScript version of handler file
+  isValidTypeScriptFile(handlerFile) {
+    //replaces the last occurance of `.js` with `.ts`, case insensitive
+    const typescriptHandlerFIle = handlerFile.replace(/\.js$/gi, ".ts");
+    return fs.existsSync(typescriptHandlerFIle);
+  }
 }
 
 module.exports = Node
