f407598f266551a794c6c541f3fa78f93eded58f
Christophe Jelger <jelger@adobe.com>
2019-08-26 10:02:16 +0200

    Remove default implicit configuration that sets the concurrency to 1 #183
    - remove implicit configurtion values for memory, concurrency, and timeout

--- compile/functions/index.js
+++ compile/functions/index.js
@@ -51,15 +51,15 @@ class OpenWhiskCompileFunctions {
   }
 
   calculateMemorySize(functionObject) {
-    return functionObject.memory || this.serverless.service.provider.memory || 256;
+    return functionObject.memory || this.serverless.service.provider.memory;
   }
 
   calculateConcurrency(functionObject) {
-    return functionObject.concurrency || this.serverless.service.provider.concurrency || 1;
+    return functionObject.concurrency || this.serverless.service.provider.concurrency;
   }
 
   calculateTimeout(functionObject) {
-    return functionObject.timeout || this.serverless.service.provider.timeout || 60;
+    return functionObject.timeout || this.serverless.service.provider.timeout;
   }
 
   calculateOverwrite(functionObject) {
